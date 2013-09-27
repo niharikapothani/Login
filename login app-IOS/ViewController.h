3fc48f00d5199cbdf75c5b2f0e27d34a29810b74
navya <navya@navya-Inspiron-N4010.(none)>
2013-09-27 20:48:31 +0530

    final commit

---
+++ login app-IOS/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+
+
+#import <UIKit/UIKit.h>
+#import "newViewController.h"
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *username;
+@property (weak, nonatomic) IBOutlet UITextField *password;
+- (IBAction)submit:(UIButton *)sender;
+
+@end
